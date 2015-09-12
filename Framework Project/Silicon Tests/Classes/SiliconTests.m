42fe04b4b3ce897b502b38a97d2d9226624d3605
Reid Main <reidmain@gmail.com>
2015-09-12 15:54:13 -0700

    Added test target.

---
+++ Framework Project/Silicon Tests/Classes/SiliconTests.m
@@ -0,0 +1,17 @@
+@import XCTest;
+
+@import Silicon;
+
+
+@interface SiliconTests : XCTestCase
+
+@end
+
+@implementation SiliconTests
+
+- (void)testExample
+{
+	XCTAssertNotNil([FDWeakMutableArray new]);
+}
+
+@end
